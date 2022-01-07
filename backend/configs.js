1b1049775a382e10138b74d968eae0ae8e9d0d38
unknown <tao32040@gmail.com>
2022-01-07 09:37:57 +0700

    Create configs.js

---
+++ backend/configs.js
@@ -0,0 +1,8 @@
+module.exports = {
+    PORT: 3015 || process.env.PORT,
+    // JOB_SCHEDULE: '*/6 * * * * *' //6วิ
+    // JOB_SCHEDULE: '40 * * * *'//ทุกนาทีที่ 40
+    // JOB_SCHEDULE: '0 1 * * *' //ทุกๆตี 1 
+    // JOB_SCHEDULE: '30-35 08 * * *' //10 โมงเช้า นาทีที่ 1-5 ให้ส่งซ้ำ
+  };
+  
